ed68da615b4cad091699048caba0f48532b0e525
khalid lafi <khalidlafi84@gmail.com>
2013-01-02 16:58:04 +0300

    removing data

--- js/wj.js
+++ js/wj.js
@@ -30,6 +30,23 @@
 
 		});
 
+
+		$('.tabs>dd').click(function() {
+
+		    var target = $(this).attr('data-tabs-target');
+
+		    if( !$(this).hasClass('active') && !($(target).hasClass('active')) ) {
+		      
+		      $('.tabs>.active').removeClass('active');
+		      $('.tabs-content>.active').removeClass('active');
+
+		      $(this).addClass('active');
+		      $(target).addClass('active').fadeIn();
+
+
+		    }
+		  });
+
 	});
 }(jQuery, window, document));
 
